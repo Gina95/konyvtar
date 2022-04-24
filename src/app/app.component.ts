7c289390d30a9224a78f0c858a052526ba770893
Georgina Bacskó <bacsko.georgina@gmail.com>
2022-04-24 14:44:46 +0200

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'konyvtar';
+  books!: Books[];
+  constructor() { }
+ 
+  ngOnInit(): void {
+    this.books = [
+      { id: 1, title: 'Jókai Mór', author: 'Árnyképek'},
+      { id: 2, title: 'Jókai Mór', author: 'Népvilág'},
+      { id: 3, title: 'Jókai Mór', author: 'Délvirágok'}
+    ]
+  }
+
+}
+interface Books {
+  id: number,
+  title: string,
+  author: string
 }
